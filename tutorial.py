5e23cd8b61e846804e1ae3a23adcd839ea1e4961
job314 <job314job@pm.me>
2022-05-24 16:51:19 +0900

    グラフ表示

--- tutorial.py
+++ tutorial.py
@@ -108,3 +108,47 @@ print('\nTest accuracy:', test_acc)
 predictions = model.predict(test_images)
 print("学習結果[予測]："+str(np.argmax(predictions[0]))) # predictions[0]最大値
 print("学習結果[正解]："+str(test_labels[0]))
+
+###正解の評価グラフ表示###
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                    100*np.max(predictions_array),
+                                    class_names[true_label]),
+                                    color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+# X個のテスト画像、予測されたラベル、正解ラベルを表示します。
+# 正しい予測は青で、間違った予測は赤で表示しています。
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
